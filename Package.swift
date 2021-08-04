e763f44e1716747a116336fa5683efbf109e4d6b
Jonathan Hemi <jonathan@hemi.dev>
2021-08-04 16:54:19 +0800

    Bump required iOS version to 14

--- Package.swift
+++ Package.swift
@@ -1,19 +1,16 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftGit2",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftGit2",
             targets: ["SwiftGit2"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(
             name: "Clibgit2",
             url: "https://github.com/yonihemi/Clibgit2",
@@ -21,8 +18,6 @@ let package = Package(
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftGit2",
             dependencies: ["Clibgit2"],
