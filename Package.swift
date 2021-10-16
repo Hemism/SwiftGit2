3432abdc3e83578299f07f5161e02f059892d728
yonihemi <jonathan@hemi.dev>
2021-10-16 19:32:54 +0800

    Bump required iOS version to 15

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftGit2",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "SwiftGit2",
